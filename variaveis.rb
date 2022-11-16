fb4cbaf55cb228fea960f89bf9c15ef2ae66dcfb
eduarda <eduardathais18@gmail.com>
2022-11-16 11:59:30 -0300

    variaveis

---
+++ variaveis.rb
@@ -0,0 +1,31 @@
+#variaveis locais x variaveis de instancia 
+
+class Cofre
+  def gerar_senha 
+    hora = Time.now.hour
+    @senha = "#{hora}#{rand(99)}" 
+  end
+
+# hora e variavel local e senha e variavel de instacia
+
+
+  def mostrar_senha 
+    puts @senha
+  end
+
+  def mostrar_hora
+    puts hora
+  end
+end
+
+  cofre = Cofre.new
+
+  cofre.gerar_senha 
+
+  cofre.mostrar_senha
+  #retorna quadro digitos que sao a hora atual por ex 11 e um numero aleatorio ate 99
+  #ainda que chamada fora do escopo, foi declarada em gerar_senha e chamada em mostrar_senha, ela retorna pois e variavel de instancia 
+
+  cofre.mostrar_hora
+  #variaveis.rb:17:in `mostrar_hora': undefined local variable or method `hora' for #<Cofre:0x0000564d863a9b78 @senha="1145"> (NameError)
+  #erro pois hora e variavel local e foi chamada fora do escopo
